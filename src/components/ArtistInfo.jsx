b334214da4dbbc8f15b2f815e7648ec9c038249a
Stephanie <tiffanieho@outlook.com>
2024-08-13 11:38:06 +0100

    added ArtistInfo.jsx index.js for export and change to App.jsx

---
+++ src/components/ArtistInfo.jsx
@@ -0,0 +1,12 @@
+
+
+const ArtistInfo = () => {
+    return (
+        <div className="artist-info">
+            <h1>Tiffjai's picks: Coldplay</h1>
+            <p>Coldplay are a British pop and rock band formed in London in 1997, consisting of vocalist and pianist Chris Martin, lead guitarist Jonny Buckland, bassist Guy Berryman, drummer and percussionist Will Champion, and manager Phil Harvey.[a] They are best known for their live performances,[3] having also impacted popular culture with their artistry, advocacy and achievements.</p>
+            <p>Citing from wiki</p>
+        </div>
+    )
+}
+export default ArtistInfo;
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as ArtistInfo } from './ArtistInfo';
